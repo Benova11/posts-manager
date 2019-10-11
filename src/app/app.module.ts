69b036e56c3566d8de43cda8e9a1d85b087aafcc
Ben <ben.afr@gmail.com>
2019-10-12 01:39:54 +0300

    added posts service,moving to adding node logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { MatInputModule, MatCardModule,MatButtonModule } from '@angular/material';
+import { MatInputModule,
+         MatCardModule,
+         MatButtonModule,
+         MatToolbarModule,
+        MatExpansionModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts_/post-create/post-create.component';
+import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './posts_/post-list/post-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent
+    PostCreateComponent,
+    PostListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -18,7 +26,9 @@ import { PostCreateComponent } from './posts_/post-create/post-create.component'
     NoopAnimationsModule,
     MatInputModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/posts_/post-list/post-list.component.ts
@@ -0,0 +1,39 @@
+import { Component , OnInit, OnDestroy } from '@angular/core';
+
+import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: 'post-list.component.html',
+  styleUrls: ['post-list.component.css']
+})
+
+export class PostListComponent implements OnInit,OnDestroy {
+  /*
+  posts = [
+    { title: 'First Post' , content: 'this is the first post\'s content' },
+    { title: 'Second Post' , content: 'this is the second post\'s content' },
+    { title: 'Third Post' , content: 'this is the third post\'s content' }
+  ];
+  */
+ posts: Post[] = [];
+ postsSubscription: Subscription;
+
+ constructor(public postsService: PostsService) {
+ }
+
+ ngOnInit() {
+    this.posts = this.postsService.getPosts();
+    this.postsSubscription = this.postsService.getPostUpdateListener()
+      .subscribe((posts: Post[]) => {
+        this.posts = posts;
+      });
+ }
+
+ ngOnDestroy() {
+   this.postsSubscription.unsubscribe();
+ }
+
+}
