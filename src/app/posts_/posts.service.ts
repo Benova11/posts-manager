545ad6a3e2105ff0b1c4772b67b35857cb13265e
Ben <ben.afr@gmail.com>
2019-10-15 19:35:48 +0300

    added mongo/mongoose

--- src/app/posts_/posts.service.ts
+++ src/app/posts_/posts.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 
 import { Post } from './post.model';
@@ -12,9 +13,18 @@ export class PostsService {
   constructor(private http: HttpClient) {}
 
   getPosts() {
-    this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
-      .subscribe((response) => {
-        this.posts = response.posts;
+    this.http.get<{message: string, posts: any}>('http://localhost:3000/api/posts')
+      .pipe(map((postData) => {
+        return postData.posts.map(post => {
+          return{
+          title: post.title,
+          content: post.content,
+          id: post._id
+          };
+        });
+      }))
+      .subscribe((transforemdPosts) => {
+        this.posts = transforemdPosts;
         this.postsUpdated.next([...this.posts]);
       });
   }
@@ -25,10 +35,20 @@ export class PostsService {
 
   addPost(title: string, content: string) {
     const newPost: Post = {id: null, title, content};
-    this.http.post<{message: string}>('http://localhost:3000/api/posts', newPost)
+    this.http.post<{message: string, postId: string}>('http://localhost:3000/api/posts', newPost)
       .subscribe(response => {
-        console.log(response.message);
+        const postId = response.postId;
+        newPost.id = postId;
         this.posts.push(newPost);
+        this.postsUpdated.next([...this.posts]);
+      });
+  }
+
+  deletePost(postId: string) {
+    this.http.delete('http://localhost:3000/api/posts/' + postId)
+      .subscribe(() => {
+        const filterdPosts = this.posts.filter(post => post.id !== postId);
+        this.posts = filterdPosts;
         this.postsUpdated.next([...this.posts]);
       });
   }
