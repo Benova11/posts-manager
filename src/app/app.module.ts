7a3ebdc1de27aaa18d486cb601dd58caa64914e7
Ben <ben.afr@gmail.com>
2019-10-16 18:40:12 +0300

    changed form to reactive approach

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule,
          MatCardModule,
@@ -26,7 +26,7 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
+    ReactiveFormsModule,
     NoopAnimationsModule,
     MatInputModule,
     MatCardModule,
